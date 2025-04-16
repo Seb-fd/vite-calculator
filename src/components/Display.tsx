8f23036da3facbadcdce89b931e7e0e0bde38440
Seb-fd <138029510+Seb-fd@users.noreply.github.com>
2025-04-16 00:56:02 -0500

    Added dynamic font size to previousOperand and currentOperand divs

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -9,12 +9,27 @@ export default function Display({
   currentOperand,
   operation,
 }: DisplayProps) {
+  const getFontSize = (text: string) => {
+    if (text.length > 18) return "text-2xl";
+    if (text.length > 12) return "text-3xl";
+    if (text.length > 8) return "text-4xl";
+    return "text-5xl";
+  };
+
   return (
-    <div className="col-span-4 h-[100px] flex flex-col items-end justify-end px-4 py-2 rounded-[1.5rem] bg-transparent text-right">
-      <div className="text-sm text-orange-300">
+    <div className="col-span-4 h-[100px] flex flex-col items-end justify-end px-4 py-2 rounded-[1.5rem] bg-transparent text-right text-wrap break-all">
+      <div
+        className={`text-sm text-orange-300 leading-none w-full overflow-hidden truncate ${getFontSize(
+          previousOperand
+        )}`}
+      >
         {previousOperand} {operation}
       </div>
-      <div className="text-5xl text-orange-400 font-semibold leading-none">
+      <div
+        className={`text-orange-400 font-semibold leading-none w-full overflow-hidden truncate ${getFontSize(
+          currentOperand || "0"
+        )}`}
+      >
         {currentOperand || "0"}
       </div>
     </div>
